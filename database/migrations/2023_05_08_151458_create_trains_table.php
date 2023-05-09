ba05c645637bf137fd85894338a28c598156ddb2
gianlucapinna99 <gianlucapinna99@gmail.com>
2023-05-09 15:40:55 +0200

    Table view

---
+++ database/migrations/2023_05_08_151458_create_trains_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+// ELEMENTI TABELLA:
+// // Ogni treno dovrà avere:
+// // Azienda
+// // Stazione di partenza
+// // Stazione di arrivo
+// // Orario di partenza
+// // Orario di arrivo
+// // Codice Treno
+// // Numero Carrozze
+// // In orario
+// // Cancellato
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 100);
+            $table->string('arrival_station', 100);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 25);
+            $table->tinyInteger('vagons')->default(1);
+            $table->boolean('on_time')->default(true);
+            $table->boolean('cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,22 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-         @vite('resources/js/app.js')
-    </head>
-    <body>
-        <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
-        </div>
 
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container">
+        <h1>Trains</h1>
+        <ul>
+            @foreach ($trains as $train)
+            <li>{{$train->train_code}} - {{$train->departure_time}} - {{$train->arrival_time}}</li>
+            @endforeach
+        </ul>
+    </div>
+</body>
+
 </html>
